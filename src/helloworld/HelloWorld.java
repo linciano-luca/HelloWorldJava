35fbb05a3b04181c675fd06819fdab7d95b94446
Luca Linciano <luca.linciano@hotmail.it>
2018-12-16 17:37:40 +0100

    First commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -7,6 +7,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Ciao mondo!");
+        System.out.println("+-----+-----+-----+-----+-----+");
+        
+        System.out.println("|  H  |  E  |  L  |  L  |  O  |");
+        System.out.println("+-----+-----+-----+-----+-----+");
     }
     
 }
